558623b81b7c31862386635e34556a6d8a146214
Metalblueberry <metalblueberry@gmail.com>
2020-05-23 10:25:25 +0200

    add more docs

--- graph_objects/plotly.go
+++ graph_objects/plotly.go
@@ -6,14 +6,17 @@ import (
 
 //go:generate sh -c "cat schema.json | plate --template main.tmpl; go fmt"
 
+// Traces is a slice of Traces
 type Traces []Trace
 
+// Fig is the base type for figures.
 type Fig struct {
 	Data   Traces  `json:"data,omitempty"`
 	Layout *Layout `json:"layout,omitempty"`
 	Config *Config `json:"config,omitempty"`
 }
 
+// AddTraces Is a shorthand  to add figures to a given figure. It handles the case where the Traces value is nil.
 func (fig *Fig) AddTraces(traces ...Trace) {
 	if fig.Data == nil {
 		fig.Data = make(Traces, 0)
@@ -27,6 +30,7 @@ type unmarshalFig struct {
 	Config *Config           `json:"config,omitempty"`
 }
 
+// UnmarshalJSON is a custom unmarshal function to properly handle special cases.
 func (fig *Fig) UnmarshalJSON(data []byte) error {
 	var err error
 	tmp := unmarshalFig{}
@@ -50,13 +54,16 @@ func (fig *Fig) UnmarshalJSON(data []byte) error {
 
 // This section is to workaround the omitempty for json serialization.
 
+// Bool represents a *bool value. Needed to tell the differenc between false and nil.
 type Bool *bool
 
 var (
 	trueValue  bool = true
 	falseValue bool = false
 
-	True  Bool = &trueValue
+	// True is a *bool with true value
+	True Bool = &trueValue
+	// False is a *bool with false value
 	False Bool = &falseValue
 )
 
