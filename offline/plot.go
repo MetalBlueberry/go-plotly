6c0b30b0802bcf45d88c634a78243a0ae6681e07
Ryan Callahan <RyanCallahan312@gmail.com>
2021-03-17 22:50:55 -0400

    Export FigToBuffer
    Exporting FigToBuffer allows for custom use cases outside just creating or serving an html file.

--- offline/plot.go
+++ offline/plot.go
@@ -19,18 +19,19 @@ type Options struct {
 
 // ToHtml saves the figure as standalone HTML. It still requires internet to load plotly.js from CDN.
 func ToHtml(fig *grob.Fig, path string) {
-	buf := figToBuffer(fig)
+	buf := FigToBuffer(fig)
 	ioutil.WriteFile(path, buf.Bytes(), os.ModePerm)
 }
 
 // Show displays the figure in your browser.
 // Use serve if you want a persistent view
 func Show(fig *grob.Fig) {
-	buf := figToBuffer(fig)
+	buf := FigToBuffer(fig)
 	browser.OpenReader(buf)
 }
 
-func figToBuffer(fig *grob.Fig) *bytes.Buffer {
+// FigToBuffer creates a byte buffer of the html file for a given figure
+func FigToBuffer(fig *grob.Fig) *bytes.Buffer {
 	figBytes, err := json.Marshal(fig)
 	if err != nil {
 		panic(err)
@@ -57,7 +58,7 @@ func Serve(fig *grob.Fig, opt ...Options) {
 		Addr:    opts.Addr,
 	}
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		buf := figToBuffer(fig)
+		buf := FigToBuffer(fig)
 		buf.WriteTo(w)
 	})
 
