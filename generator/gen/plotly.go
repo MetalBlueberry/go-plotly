ca52f91d856b026150dcc2bd7db41ee82112b10c
Metalblueberry <metalblueberry@gmail.com>
2021-05-09 20:25:41 +0200

    add some descriptions

--- generator/gen/plotly.go
+++ generator/gen/plotly.go
@@ -47,12 +47,14 @@ var (
 	False Bool = &falseValue
 )
 
+// String is a string value, numeric values are converted to string by plotly
 type String interface{}
 
-type DataArray interface{}
-
+// Color A string describing color. Supported formats: - hex (e.g. '#d3d3d3') - rgb (e.g. 'rgb(255, 0, 0)') - rgba (e.g. 'rgb(255, 0, 0, 0.5)') - hsl (e.g. 'hsl(0, 100%, 50%)') - hsv (e.g. 'hsv(0, 100%, 100%)') - named colors (full list: http://www.w3.org/TR/css3-color/#svg-color)",
 type Color interface{}
 
+// ColorList A list of colors. Must be an {array} containing valid colors.
 type ColorList []Color
 
+// ColorScale A Plotly colorscale either picked by a name: (any of Greys, YlGnBu, Greens, YlOrRd, Bluered, RdBu, Reds, Blues, Picnic, Rainbow, Portland, Jet, Hot, Blackbody, Earth, Electric, Viridis, Cividis ) customized as an {array} of 2-element {arrays} where the first element is the normalized color level value (starting at *0* and ending at *1*), and the second item is a valid color string.
 type ColorScale interface{}
