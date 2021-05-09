2fb1ecedff1cde4e52618851d382cdcc7c364eb0
Metalblueberry <metalblueberry@gmail.com>
2021-05-09 20:28:13 +0200

    add generator command

--- generator/cmd/generator/main.go
+++ generator/cmd/generator/main.go
@@ -1,24 +1,56 @@
 package main
 
 import (
+	"flag"
+	"io"
+	"log"
 	"os"
 
 	"github.com/MetalBlueberry/go-plotly/generator"
 )
 
+type Creator struct{}
+
+func (c Creator) Create(name string) (io.WriteCloser, error) {
+	return os.Create(name)
+}
+
 func main() {
-	file, err := os.Open("schema.json")
+	schema := flag.String("schema", "schema.json", "plotly schema")
+	outputDirectory := flag.String("output-directory", "gen/", "output directory, must exist before generation")
+
+	flag.Parse()
+
+	file, err := os.Open(*schema)
 	if err != nil {
-		panic(err)
+		log.Fatalf("unable to open schema, %s", err)
 	}
+
 	root, err := generator.LoadSchema(file)
 	if err != nil {
-		panic(err)
+		log.Fatalf("unable to load schema, %s", err)
 	}
-	r, err := generator.NewRenderer("templates/*.tmpl", root)
+
+	r, err := generator.NewRenderer(Creator{}, root)
 	if err != nil {
+		log.Fatalf("unable to create a new renderer, %s", err)
 		panic(err)
 	}
 
-	r.WriteTraces(os.Stdout)
+	output := *outputDirectory
+
+	err = r.WriteTraces(output)
+	if err != nil {
+		log.Fatal("unable to write traces, %w", err)
+	}
+
+	err = r.WriteLayout(output)
+	if err != nil {
+		log.Fatal("unable to write layout, %w", err)
+	}
+
+	err = r.WriteConfig(output)
+	if err != nil {
+		log.Fatal("unable to write config, %w", err)
+	}
 }
