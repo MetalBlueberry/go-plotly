d9c5d51c99ff0e9eea4cb0f11b448e567452d1d8
metalblueberry <metalblueberry@gmail.com>
2024-05-22 22:57:11 +0200

    format

--- types/arrayok.go
+++ types/arrayok.go
@@ -1,38 +1,40 @@
 package types
 
 import (
-    "encoding/json"
-    "reflect"
+	"encoding/json"
+	"reflect"
 )
 
+// ArrayOK is a type that allows you to define a single value or an array of values, But not both.
+// If Array is defined, Value will be ignored.
 type ArrayOK[T any] struct {
-    Value T
-    Array []T
+	Value T
+	Array []T
 }
 
 func (arrayOK *ArrayOK[T]) MarshalJSON() ([]byte, error) {
-    if arrayOK.Array!= nil {
-        return json.Marshal(arrayOK.Array)
-    }
-    return json.Marshal(arrayOK.Value)
+	if arrayOK.Array != nil {
+		return json.Marshal(arrayOK.Array)
+	}
+	return json.Marshal(arrayOK.Value)
 }
 
 func (arrayOK *ArrayOK[T]) UnmarshalJSON(data []byte) error {
-    arrayOK.Array = nil
-    arrayOK.Value = reflect.Zero(reflect.TypeOf(arrayOK.Value)).Interface().(T)
+	arrayOK.Array = nil
+	arrayOK.Value = reflect.Zero(reflect.TypeOf(arrayOK.Value)).Interface().(T)
 
-    var array []T
-    err := json.Unmarshal(data, &array)
-    if err == nil {
-        arrayOK.Array = array
-        return nil
-    }
+	var array []T
+	err := json.Unmarshal(data, &array)
+	if err == nil {
+		arrayOK.Array = array
+		return nil
+	}
 
-    var value T
-    err = json.Unmarshal(data, &value)
-    if err!= nil {
-        return err
-    }
-    arrayOK.Value = value
-    return nil
+	var value T
+	err = json.Unmarshal(data, &value)
+	if err != nil {
+		return err
+	}
+	arrayOK.Value = value
+	return nil
 }
